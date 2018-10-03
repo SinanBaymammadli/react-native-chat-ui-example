b36feb47e2f5e79a50539a781bbef48e54ffa33c
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2018-10-03 18:09:31 +0400

    update to 0.1.6

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import ChatUI from "react-native-chat-ui";
+
+const messageData = [
+  {
+    id: "1",
+    userId: "1",
+    text: "Hello world",
+    error: false,
+    sending: false,
+    createdAt: "2018-10-03 12:13:24",
+  },
+  {
+    id: "2",
+    userId: "2",
+    text: "Hello world",
+    error: false,
+    sending: false,
+    createdAt: "2018-08-03 12:13:24",
+  },
+];
 
 export default class App extends Component {
-  componentDidMount = () => {};
+  state = {
+    messages: [],
+  };
+
+  componentDidMount = () => {
+    this.getMessages();
+  };
+
+  getMessages = () => {
+    this.setState({
+      messages: messageData,
+    });
+  };
+
+  sendMessage = message => {
+    this.setState(prevState => ({
+      messages: [message, ...prevState.messages],
+    }));
+  };
 
   render() {
+    const { messages } = this.state;
+
     return (
       <View
         style={{
           flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#F5FCFF",
         }}
       >
-        <Text>Welcome to React Native!</Text>
+        <ChatUI
+          messages={messages}
+          user={{
+            id: "1",
+          }}
+          chatter={{
+            id: "2",
+          }}
+          onSend={this.sendMessage}
+        />
       </View>
     );
   }
